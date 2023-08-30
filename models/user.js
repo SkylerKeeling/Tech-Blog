95100874fd868de9206a7d4ae324a48441123b8d
skyler <skyler@skylers-MacBook-Pro.local>
2023-08-30 14:59:41 -0300

    models created

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const {Model, DataTypes} = require("sequelize")
+const sequelize = require("../config/connection")
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "gallery",
+  }
+)
+
+module.exports = User
