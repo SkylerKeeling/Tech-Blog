d459c571d558ccde986f38942cbdd588c285ce8a
skyler <skyler@skylers-MacBook-Pro.local>
2023-09-07 11:28:33 -0300

    main handlebars adjusted for styling

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 
-const userRoutes = require("./user-routes.js")
+const userRoutes = require("./userRoutes.js")
 const postRoutes = require("./postroutes")
 
 router.use("/post", postRoutes)
